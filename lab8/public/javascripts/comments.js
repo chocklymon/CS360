2a84d4c8f60cc672404ffae64244b2c8eb12a6ba
Curtis Oakley <coakley@chocklydigital.com>
2016-03-13 22:56:53 -0600

    Add the basics of lab8.

---
+++ lab8/public/javascripts/comments.js
@@ -0,0 +1,68 @@
+/**
+ * Comments (Lab8) front end UI JS
+ */
+jQuery(document).ready(function($) {
+    // Configuration
+    var comentAPIurl = 'comment';
+
+    // Event handling
+    $('#commentForm').on('submit', function(event) {
+        // Prevent the form from submitting
+        event.preventDefault();
+
+        var nameElm =  $("#Name"),
+            commentElm = $("#Comment");
+        var name = nameElm.val();
+        var comment = commentElm.val();
+        if (name == '') {
+            nameElm.parent().addClass('has-error');
+        } else {
+            nameElm.parent().removeClass('has-error');
+        }
+        if (comment == '') {
+            commentElm.parent().addClass('has-error');
+        } else {
+            commentElm.parent().removeClass('has-error');
+        }
+        if (name == '' || comment == '') {
+            $('#submitMsg').text('Please fill in all required fields.');
+            return false;
+        }
+
+        // Serialize the comment
+        var jsObj = JSON.stringify({
+            Name: name,
+            Comment: comment
+        });
+        $("#json").text(jsObj);
+
+        // Post the comment
+        $.ajax({
+            url: comentAPIurl,
+            type: 'POST',
+            data: jsObj,
+            contentType: 'application/json; charset=utf-8'
+        }).then(
+            function(data, textStatus) {
+                $("#submitMsg").text(textStatus);
+            },
+            function(error) {
+                console.warn('Problem posting comments: ', error);
+                $('#submitMsg').text('Error');
+            }
+        );
+    });
+
+    $('#getCommentsBtn').click(function() {
+        $.getJSON(comentAPIurl, function(data) {
+            //console.log(data);
+            var commentList = $('<ul/>');
+            $.each(data, function(i, comment) {
+                commentList.append(
+                    $('<li>').text('Name: ' + comment.Name + ' -- Comment: ' + comment.Comment)
+                );
+            });
+            $("#comments").html(commentList);
+        });
+    });
+});
